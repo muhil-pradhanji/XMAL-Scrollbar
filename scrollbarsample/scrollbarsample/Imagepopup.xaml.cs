569f689bf5d4725486c17d7a691052f915e42e7d
PRADHANJI <114460952+muhil-pradhanji@users.noreply.github.com>
2024-12-10 11:42:48 +0530

    Add files via upload

---
+++ scrollbarsample/scrollbarsample/Imagepopup.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace scrollbarsample
+{
+    /// <summary>
+    /// Interaction logic for Imagepopup.xaml
+    /// </summary>
+    public partial class Imagepopup : Window
+    {
+        public Imagepopup()
+        {
+            InitializeComponent();
+         
+
+        }
+        //private void BrightnessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        //{
+        //    if (DisplayedImage != null) // Check if the image is initialized
+        //    {
+        //        DisplayedImage.Opacity = BrightnessSlider.Value / 100.0;
+        //        sliderValueText.Text = "Brightness: " + (int)BrightnessSlider.Value;
+        //    }
+        //}
+
+        private void Scroll_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (DisplayedImage != null) // Check if the image is initialized
+            {
+                DisplayedImage.Opacity = Scroll.Value / 100.0;
+                sliderValueText.Text = "Brightness: " + (int)Scroll.Value;
+                
+            }
+        }
+    }
+}
---
+++ scrollbarsample/scrollbarsample/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+namespace scrollbarsample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // Ensure the slider is not null and update the TextBlock with the current value
+            //Debug.WriteLine("Slider Value Changed: " + e.NewValue);
+            //sliderValueText.Text = ((int)mySlider.Value).ToString();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Imagepopup popup = new Imagepopup();
+            popup.ShowDialog();
+        }
+    }
+}
